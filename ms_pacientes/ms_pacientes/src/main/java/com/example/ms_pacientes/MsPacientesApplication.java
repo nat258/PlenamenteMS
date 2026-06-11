47c0a1015755bbb212d120b91399735024550da7
Nataly Arriaza <nat.arriaza@duocuc.cl>
2026-06-10 22:38:52 -0400

    Primer commit: Todos los microservicios en su sitio

---
+++ ms_pacientes/ms_pacientes/src/main/java/com/example/ms_pacientes/MsPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPacientesApplication.class, args);
+	}
+
+}
---
+++ ms_profesionales/ms_profesionales/src/main/java/com/example/ms_profesionales/MsProfesionalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_profesionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProfesionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProfesionalesApplication.class, args);
+	}
+
+}
