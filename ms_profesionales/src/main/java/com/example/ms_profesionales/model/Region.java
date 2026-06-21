765aa922f461afeaad60cbb66e9c2351dc9b8af0
Nataly Arriaza <nat.arriaza@duocuc.cl>
2026-06-20 20:25:05 -0400

    se agrego consulta sql para crear tabla comuna, se agrego relacion entre comuna y region, se agrego relacion entre psicologo y comuna, se agrego relacion entre sucursal y comuna, se agrego relacion entre sucursal y region, se agrego relacion entre sucursal y psicologo, se agrego relacion entre especialidad y psicologo, se agrego relacion entre especialidad y sucursal, se modifico clase region para agregar lista de comunas, se modifico clase psicologo para agregar comuna, se modifico clase sucursal para agregar comuna, region y psicologos, se modifico clase especialidad para agregar lista de psicologos y lista de sucursales. Ademas se agrego @Colums a cada uno de las clases para identificar el nombre de cada columna en la base de datos, se agrego @JoinColumn para identificar las columnas que hacen referencia a otras tablas, se agrego @ManyToOne para identificar las relaciones entre psicologo y comuna, sucursal y comuna, sucursal y region, sucursal y psicologo, se agrego @ManyToMany para identificar las relaciones entre especialidad y psicologo, especialidad y sucursal.

--- ms_profesionales/src/main/java/com/example/ms_profesionales/model/Region.java
+++ ms_profesionales/src/main/java/com/example/ms_profesionales/model/Region.java
@@ -25,10 +25,11 @@ public class Region {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_region")
     private Integer id;
 
     @NotBlank(message = "El nombre de region no puede estar vacio!")
-    @Column(unique = true)
+    @Column(name = "nombre_region", unique = true)
     private String nombre;
 
     @OneToMany(mappedBy = "region")
